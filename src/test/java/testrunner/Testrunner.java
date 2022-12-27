26f28d24d25c8597866c45c873201fadab70d543
smitha <smithavarghese99@gmail.com>
2022-12-27 14:39:11 +0530

    updated testrunner

--- src/test/java/testrunner/Testrunner.java
+++ src/test/java/testrunner/Testrunner.java
@@ -13,7 +13,7 @@ import io.cucumber.junit.CucumberOptions;
 @RunWith(Cucumber.class)
 @CucumberOptions(
 				features="src/test/java/features",
-				glue="stepdefiniton")
+				glue="stepdefiniton",plugin = {"json:target/cucumber.json"})
 public class Testrunner  {
 
 }
